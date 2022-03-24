087c6aaa932447fbbaa8dce44cc8745d3e7f19f3
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-24 15:51:37 +0300

    Prettier fix

--- app/Controllers/Http/BaseController.ts
+++ app/Controllers/Http/BaseController.ts
@@ -1,17 +1,17 @@
-enum STATE_CODE {
-    EMPTY = 'EMPTY',
-    HOLIDAY = 'HOLIDAY',
-    SUCCESS = 'SUCCESS',
+enum StateCodes {
+  EMPTY = 'EMPTY',
+  HOLIDAY = 'HOLIDAY',
+  SUCCESS = 'SUCCESS',
 }
 
 export default class BaseController {
-    constructor() {}
+  constructor() {}
 
-    /** State codes (NOT HTTP codes) */
-    readonly STATE = STATE_CODE;
+  /** State codes (NOT HTTP codes) */
+  protected readonly STATE = StateCodes
 
-    /** Get state object */
-    protected getCodeState(stateCode: keyof typeof this.STATE) {
-        return { status: stateCode }
-    }
+  /** Get state object */
+  protected getCodeState(stateCode: keyof typeof this.STATE) {
+    return { status: stateCode }
+  }
 }
