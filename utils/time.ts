8dbd3ff9d6318c2c0a14ae6c578fc2efcd5e8cd1
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-24 16:40:42 +0300

    Fix

--- utils/time.ts
+++ utils/time.ts
@@ -2,4 +2,4 @@
  * Returns true, if input date is holiday
  * @param dateToCompare - Input date
  */
- export const isHoliday = (dateToCompare) => Boolean(dateToCompare === 5 || dateToCompare === 6);
+export const isHoliday = (dateToCompare) => Boolean(dateToCompare === 5 || dateToCompare === 6)
