39c3cbc0bca758076ed4b4f3e084bcfa15e5d525
Vadim Ignatov <ignatov0131@gmail.com>
2022-06-08 12:24:09 +0300

    Update user model & service

--- app/Services/User/index.ts
+++ app/Services/User/index.ts
@@ -1,5 +1,10 @@
-import User from "App/Models/Mongoose/User"
+import User from 'App/Models/User'
+import Group from 'App/Models/Group'
 
 export const updateUserGroup = async (id, newGroup) => {
-    await User.findOneAndUpdate({ userID: id }, { group: newGroup });
+  const group = await Group.query().select('id').where('name', '=', newGroup).limit(1)
+
+  if (group) {
+    await User.updateOrCreate({ tgId: id }, { tgId: id, groupName: group[0].id })
+  }
 }
