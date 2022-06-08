4346ea1cd0817d28fc5038d5704ad20f1e310931
Vadim Ignatov <ignatov0131@gmail.com>
2022-06-08 10:17:11 +0300

    Comment mongoose driver

--- providers/MongoProvider.ts
+++ providers/MongoProvider.ts
@@ -1,16 +1,16 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
-import { Mongoose } from 'mongoose';
+import { Mongoose } from 'mongoose'
 import Env from '@ioc:Adonis/Core/Env'
 
 export default class MongoProvider {
   constructor(protected app: ApplicationContract) {}
 
   public async register() {
-    const mongoose = new Mongoose();
+    const mongoose = new Mongoose()
 
-    await mongoose.connect(Env.get('MONGODB_CONNECTION'))
-    
-    this.app.container.singleton('Mongoose', () => mongoose);
+    // await mongoose.connect(Env.get('MONGODB_CONNECTION'))
+
+    this.app.container.singleton('Mongoose', () => mongoose)
   }
 
   public async boot() {}
