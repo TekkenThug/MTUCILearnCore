0d4dc324755c3b1909398a2d4fa48a927458d961
Vadim Ignatov <ignatov0131@gmail.com>
2022-06-08 11:00:10 +0300

    Create user migration & model

---
+++ database/migrations/1654674189217_users.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('tg_id').unique()
+      table.integer('group_name').unsigned()
+      table.foreign('group_name').references('groups.id')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
